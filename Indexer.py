0764b710c84fd659e1548a0424a7bcb1e35fb383
Ronnyv <ronny_vega@icloud.com>
2015-05-16 21:54:16 -0700

    Finished Indexer.py, made edits in Design.txt, and wrote a test Driver which built a test index

--- Indexer.py
+++ Indexer.py
@@ -1,15 +1,21 @@
+from collections import defaultdict
 import time
 
 class Indexer():
 	def __init__(self, tokens):
-		self.dictionary = {} 
-		self.tokenList = tokens
+		self.__dict = defaultdict(list)
+		self.__tokenList = tokens
 	
-	def BuildIndex():
-		postingsFile = open("index.txt", 'w')
-		t0 = time.clock()
-		for t in self.tokenList:
-			postingsList = []
-			t.split()
-			if dictionary.has_key(t[0]):
-				print ("hello")
+	def BuildIndex(self, fileName):
+		index = open(fileName, 'w')
+		self.t0 = time.clock()
+		for t in self.__tokenList:
+			print("t is " + str(t))
+			t = t.split()
+			print("t split is " + str(t))
+			self.__dict[t[0]].append(t[1])
+		index.write(str(self.__dict))
+		index.close()
+		self.t1 = time.clock()
+		return self.t1-self.t0
+
