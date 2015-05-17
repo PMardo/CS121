68f4a9419d2d48b048f33140fa9e3f6a33532662
Ronnyv <ronny_vega@icloud.com>
2015-05-16 22:21:36 -0700

    fixed typos in Design.txt and finished Reporter.py

--- Driver.py
+++ Driver.py
@@ -1,6 +1,6 @@
 #from Parser import Parser
 from Indexer import Indexer
-#from Reporter import Reporter
+from Reporter import Reporter
 
 indexer = Indexer(["hello.title doc0.3", "world.text doc1.54", "hello.title doc1.2"])
 time_taken = indexer.BuildIndex("index.txt")
--- Reporter.py
+++ Reporter.py
@@ -1,1 +1,20 @@
 import os
+
+class Reporter():
+	def __init__(self, a, b, c, d):
+		self.docs = a
+		self.unique = b
+		self.index = c
+		self.time = d
+
+	def Report(self):
+		report = open("Report.txt", "w")
+		report.write("1. There are %d documents in the corpus.\n",self.docs)
+		report.write("2. There are %d unique words in the corpus.\n",self.unique)
+		report.write("3. The size of the index on disk is %dKB\n",os.path.getsize(index)/1000)
+		report.write("4. The time taken to build the index was %d seconds\n",self.time)
+		report.close()
+
+
+
+
