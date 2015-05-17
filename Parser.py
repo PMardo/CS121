e24f8391a18bbf5a6858131f81ac8e1cf251273c
PatriceMardo <pmardo@uci.edu>
2015-05-17 15:59:04 -0700

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -1,2 +1,21 @@
 import json
 import nltk
+## Still Editing this (need to figure out json) - Patrice 
+class parser():
+    def __init__(self):
+        self.__dict = defaultdict(list)
+        #self.__corpusPath = corpusPath
+        
+    def startParse(self, corpusPath):
+        folder=os.listdir(corpusPath)
+        num_docs = len([d for d in folder if d.endswith('.txt')]) 
+
+        for d in folder:
+            if d.endswith('txt'):
+                try:
+                    file=open(d,'r')
+                    print(file)
+                finally:
+                    file.close()
+            else:
+                continue
