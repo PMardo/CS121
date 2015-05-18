15f14f643bcf7851c1c8937a3ef55aaacde5fd09
PatriceMardo <pmardo@uci.edu>
2015-05-18 01:03:12 -0700

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -19,21 +19,31 @@ class parser():
         self.parsed_list=[]
         
     def startParse(self, corpusPath):
-        num_docs = len([d for d in os.listdir(corpusPath) if d.endswith('.txt')]) 
+        num_docs = len([d for d in os.listdir(corpusPath) if d.endswith('.txt')]) # count docs in corpus, exclude non txt files
         num_unique=0
-        s=''
-        
+        finallist=[]
+        tempstring=''
         for root, dirs, files in os.walk(corpusPath):
             for file in files:
                 if file.endswith(".txt"):
                     data = json.loads(open(os.path.join(root,file)).read())
-                    docid = data.items()[0]
                     title = data.items()[-1] #does text include title?
+                    docid = data.items()[0]
                     text = data.items()[1]
-                    tokenized_text = word_tokenize(str(text[1])) #tokenize text str after converting from unicode
-                    num_unique = len(set(t.lower() for t in tokenized_text if not t in stopwords.words('english')))
-            
+                    tokenized_text = word_tokenize(str(text[1])) #tokenize text str after converting from unicode  
+                    words = [w.lower() for w in tokenized_text if not w in stopwords.words('english')]
+                    
+                    num_unique += len(set(words)) # Num of unique words in entire corpus 
+                  
+                    freq = {} # Get frequencies
+                    for w in words:
+                        if freq.has_key(w):
+                            freq[w] = freq[w] + 1
+                        else:
+                            freq[w] = 1
+#         Format: "hello.title doc1.2"                   
                 else: continue
+
         return num_docs,num_unique
 
     
