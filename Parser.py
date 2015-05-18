7f5a1e2c6bb83708e06c8db27645c7bf66449b15
PatriceMardo <pmardo@uci.edu>
2015-05-17 18:32:44 -0700

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-import os, json, nltk
 #     Description
 #         This component parses and tokenizes the corpus into a list of "term.field docId.frequency" tokens, 
 #         where stop words are discarded. Once the entire corpus has been tokenized, this list is returned 
@@ -12,18 +10,31 @@ import os, json, nltk
 # p=Parser.parser()
 # p.startParse(corpusPath)
 
+import os, json
+from nltk import word_tokenize
+from nltk.corpus import stopwords
 class parser():
     def __init__(self):
-        self.parsed_list=[] # Still need to work on adding to this list...
+        self.parsed_list=[]
+        
     def startParse(self, corpusPath):
         num_docs = len([d for d in os.listdir(corpusPath) if d.endswith('.txt')]) 
+        num_unique=0
         s=''
+        
         for root, dirs, files in os.walk(corpusPath):
             for file in files:
                 if file.endswith(".txt"):
                     data = json.loads(open(os.path.join(root,file)).read())
                     docid = data.items()[0]
+                    title = data.items()[-1] #does text include title?
                     text = data.items()[1]
-                    title = data.items()[-1]      
+                    tokenized_text = word_tokenize(str(text[1])) #tokenize text str after converting from unicode
+                    num_unique = len(set(t.lower() for t in tokenized_text if not t in stopwords.words('english')))
+            
                 else: continue
-        return num_docs
+        return num_docs,num_unique
+
+    
+
+
