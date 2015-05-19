d0793ee8f48890ca2ac2045aae5fe9548bd10c67
Ronnyv <ronny_vega@icloud.com>
2015-05-19 15:32:47 -0700

    report.txt and index completed

--- Driver.py
+++ Driver.py
@@ -3,16 +3,16 @@ from Indexer import Indexer
 from Reporter import Reporter
 import time
 
-Parser = Parser()
-Indexer = Indexer()
-Reporter = Reporter()
-
 corpuspath = "FileDump"
 indexfile = "index.txt"
 reportfile = "Report.txt"
 
+Parser = Parser()
+Indexer = Indexer()
+Reporter = Reporter()
+
 t0 = time.clock()
 metatuple = Parser.ParseCorpus(corpuspath)
-indextime = Indexer.BuildIndex(metatuple[2], indexfile)
+Indexer.BuildIndex(metatuple[2], indexfile)
 t1 = time.clock()
 Reporter.Report(metatuple[0], metatuple[1], indexfile, t1-t0, reportfile)
